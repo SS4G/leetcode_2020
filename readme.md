7f4e9cf7f267a812483e00f49ad9942382d5310f
zihengs <zihengs@opera.com>
2020-01-02 14:24:26 +0800

    test readme.md

--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
-## readme
-- src 文件夹下面是各种语言的刷题源代码
-- lib 是各种可能用到的常用脚手架代码 或者 工具代码
-- 刷题语言 包括但不限于 C C++ Python Go Java 当然大部分应该是Python
-- 2020 年 每天三个题 争取 年底完成1000道 不够的要及时补上
-- 全部题目位于leetcode 中文版
-[LeetCode-CN](https://leetcode-cn.com/problemset/algorithms/)
+## Readme
+1. src 文件夹下面是各种语言的刷题源代码
+2. lib 是各种可能用到的常用脚手架代码 或者 工具代码
+3. doc 存放比较有代表性的题目的题解 使用markdown 编写
+4. 刷题语言 包括但不限于 C C++ Python Go Java 当然大部分应该是Python
+5. 2020 年 每天三个题 争取 年底完成至1024道 不够的要及时补上 比如今天是2020年第3天那么在记录上就要完成到至少第9个题
+6. 全部题目位于leetcode 中文版 [LeetCode-CN](https://leetcode-cn.com/problemset/algorithms/) 当然题解应该参考下英文版 [LeetCode-EN](https://leetcode.com/)
+
+## Record
+progress now **30/1024**
+[img](http://progressed.io/bar/28)
--- src/l0001.py
+++ src/l0001.py
@@ -1,2 +1,20 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+from collections import defaultdict
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        rec = defaultdict(list)
+        for idx, i in enumerate(nums):
+            rec[i].append(idx)
+        
+        for k in rec.keys():
+            if 2 * k == target:
+                if len(rec[k]) == 2:
+                    return rec[k]
+            else:
+                if target - k in rec:
+                    return [rec[k][0], rec[target- k][0]]
+        return [-1, -1]
